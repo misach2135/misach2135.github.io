901b90c724a1dad6f7ecf4ca74cf97e0b71bc0c6
Nikita Isachenko <misachenko2135@gmail.com>
2026-04-09 14:15:50 +0300

    feat: add 404 page

---
+++ src/pages/NotFound.tsx
@@ -0,0 +1,11 @@
+import { Component } from "solid-js";
+
+const NotFound: Component = () => {
+  return (
+    <div>
+      <h1>404 Not found.</h1>
+    </div>
+  );
+};
+
+export default NotFound;
--- src/router.tsx
+++ src/router.tsx
@@ -3,12 +3,14 @@ import { Route, Router as SolidRouter } from "@solidjs/router";
 
 import CV from "./pages/CV";
 import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
 
 const Router: Component = () => {
   return (
     <SolidRouter>
       <Route path="/" component={Home} />
       <Route path="/cv" component={CV} />
+      <Route path="*404" component={NotFound} />
     </SolidRouter>
   );
 };
